c5012af339804d78734778de62240b55da3c513d
25ERA01-ErickRamirez <eramirezdb25@gmail.com>
2026-06-18 12:04:47 -0600

    Add files via upload

---
+++ Lecc31,Actvidad1,pag 114.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int filas = 3;
+        int columnas = 5;
+        int i = 0; int j = 0;
+        int[,] matriz1 = new int[3, 5];
+
+        for (i = 0; i < filas; i++) ;
+        {
+            for (j = 0; i < columnas; j++) ;
+            {
+
+                Console.WriteLine("Ingresa el numero que quieres guardar en, la Fila. " + i + ", Columna: " + j + "=");
+                matriz1[i, j] = int.Parse(Console.ReadLine());
+
+
+            }
+        }
+        for (i = 0; i < filas; i++)
+        {
+            for (j = 0; j < columnas; j++)
+            {
+
+                Console.WriteLine("Fila: " + i + ", Columna: " + j + " =" + matriz1[i, j]);
+
+
+
+            }
+        }
+    }
+}
